ab19119f678722afd12c15efa5c9c5beb3387f51
Vitaliy200989 <vchernykh@emdev.ru>
2022-07-12 10:02:45 +0700

    12.07.2022 dev contacts feature

--- src/main/java/com/example/todogo/controllers/ContactsController.java
+++ src/main/java/com/example/todogo/controllers/ContactsController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class ContactsController {
 
     @GetMapping("/myContacts")
-    public String toCalendarPage() {
+    public String toContactsPage() {
         return TodoGoConstants.USER_CONTACTS_PAGE;
     }
 }
